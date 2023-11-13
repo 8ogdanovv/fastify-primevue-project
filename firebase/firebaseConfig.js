b18577ba3db918ddf5586d85a828cceed9fa3859
vadym4che <vadym4che@gmail.com>
2023-11-13 15:55:43 +0200

    prepare to test deploy

---
+++ firebase/firebaseConfig.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyD6FbbqTiUzpv7wVz39GMxuGPkVSwT2KZI', // import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: 'vue3-feedback-widget-eb813.firebaseapp.com', // import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: 'vue3-feedback-widget-eb813', // import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: 'vue3-feedback-widget-eb813.appspot.com', // import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: '990870529074', // import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: '1:990870529074:web:be8b4aefaf89385d16882b', // import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: 'G-1G1WQDDXC1' // import.meta.env.VITE_FIREBASE_MEASUREMENT_ID // optional
+}
+
+export default firebaseConfig
